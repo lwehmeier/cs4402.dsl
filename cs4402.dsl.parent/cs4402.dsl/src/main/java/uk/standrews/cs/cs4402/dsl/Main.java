1c7ff60567faa4c6648696d65226d4f2e3264d44
Leon Wehmeier <wehmieer@ovgu.de>
2018-04-16 02:21:32 +0100

    merged dsl and solver

--- cs4402.dsl.parent/cs4402.dsl/src/main/java/uk/standrews/cs/cs4402/dsl/Main.java
+++ cs4402.dsl.parent/cs4402.dsl/src/main/java/uk/standrews/cs/cs4402/dsl/Main.java
@@ -2,10 +2,16 @@ package uk.standrews.cs.cs4402.dsl;
 
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.parser.IParser;
 import org.eclipse.xtext.parser.ParseException;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
+import uk.standrews.cs.cs4402.dsl.dSL.CSP;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -15,41 +21,16 @@ import java.io.Reader;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!!");
-        Main xtp = new Main();
-        try {
-            EObject root = xtp.parse(new FileReader("/cs/home/lw96/IdeaProjects/cs4402.dsl/test.bcsp"));
-            System.out.println(root);
-        }
-        catch (IOException ex){
-            ex.printStackTrace();
-        }
-    }
-    @Inject
-    public IParser parser;
 
-    public Main() {
-        setupParser();
+        System.out.println(parse());
     }
-
-    private void setupParser() {
+    public static CSP parse(){
         Injector injector = new DSLStandaloneSetup().createInjectorAndDoEMFRegistration();
-        injector.injectMembers(this);
-    }
-
-    /**
-     * Parses data provided by an input reader using Xtext and returns the root node of the resulting object tree.
-     * @param reader Input reader
-     * @return root object node
-     * @throws IOException when errors occur during the parsing process
-     */
-    public EObject parse(Reader reader) throws IOException
-    {
-        IParseResult result = parser.parse(reader);
-        if(result.hasSyntaxErrors())
-        {
-            throw new ParseException("Provided input contains syntax errors.");
-        }
-        return result.getRootASTElement();
+        XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+        resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+        Resource resource = resourceSet.getResource(
+                URI.createURI("../test.bcsp"), true);
+        EObject root = resource.getContents().get(0);
+        return (CSP)root;
     }
 }
